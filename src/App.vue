c92e1bb35cfb6f9491fe5f806def5997aa58b346
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2021-12-16 17:09:25 +0300

    v

--- src/App.vue
+++ src/App.vue
@@ -471,8 +471,8 @@ export default {
 
 	created() {
 
-    	this.pocketnet = false
-		this.mobile = !this.pocketnet
+    	/*this.pocketnet = false
+		this.mobile = !this.pocketnet*/
 
 		this.$store.commit('setPocketnet', this.pocketnet);
 		this.$store.commit('setMobile', this.mobile);
