d4308946308f0a41942f3bc5b7d2add52069be7a
Maksim Muhamedzyanov <maksim@Maksims-MacBook-Pro.local>
2022-07-27 14:35:15 +0500

    make resending voice message on error

--- vue.config.js
+++ vue.config.js
@@ -1,53 +1,51 @@
 var prependcssvars = `@import "@/styles/variables/common.sass"; @import "@/styles/mixins/common.sass";`;
-const path = require('path');
+const path = require("path");
 module.exports = {
-	devServer: {
-		open: process.platform === "darwin",
-		host: "0.0.0.0",
-		port: 7001, // CHANGE YOUR PORT HERE!
-		https: false,
-		hotOnly: false,
-	},
+  devServer: {
+    open: process.platform === "darwin",
+    host: "0.0.0.0",
+    port: 7001, // CHANGE YOUR PORT HERE!
+    https: true,
+    hotOnly: false,
+  },
 
-	publicPath: "/",
-	lintOnSave: false,
+  publicPath: "/",
+  lintOnSave: false,
 
-	css: {
-		loaderOptions: {
-			sass: {
-				prependData: prependcssvars,
-			},
-		},
-	},
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: prependcssvars,
+      },
+    },
+  },
 
-	runtimeCompiler: true,
-	"transpileDependencies": [
-		"rxjs-interop"
-	],
-	configureWebpack: {
-		resolve: {
-			extensions: [".js", ".ts"],
-		},
-		output: {
-			pathinfo: false,
-		},
+  runtimeCompiler: true,
+  transpileDependencies: ["rxjs-interop"],
+  configureWebpack: {
+    resolve: {
+      extensions: [".js", ".ts"],
+    },
+    output: {
+      pathinfo: false,
+    },
 
-		module: {
-			rules: [
-				{
-					loader: "babel-loader",
-					test: "/.(js)$/",
-					include: [path.resolve('node_modules/rxjs-interop')]
-				},
-				{
-					test: /\.(ts|tsx)?$/,
-					use: "ts-loader",
-				},
-				{
-					test: /\.wasm$/,
-					loaders: ["wasm-loader"],
-				},
-			],
-		},
-	},
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: "/.(js)$/",
+          include: [path.resolve("node_modules/rxjs-interop")],
+        },
+        {
+          test: /\.(ts|tsx)?$/,
+          use: "ts-loader",
+        },
+        {
+          test: /\.wasm$/,
+          loaders: ["wasm-loader"],
+        },
+      ],
+    },
+  },
 };
