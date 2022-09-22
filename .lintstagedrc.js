a568c8f56f3dcf8920f7f0bfc668daba045e6860
Michael L <levinez.michael@gmail.com>
2022-09-22 18:29:24 +0500

    remake all to husky

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,8 @@
 module.exports = {
-  "src/**/*.js": ["yarn run lint:js"],
+  'src/**/*.{js, vue}': [
+    "prettier --write",
+    "eslint --fix src/ !src/matrix-js-sdk/",
+    "yarn run lint:js",
+    "git add"
+  ],
 };
