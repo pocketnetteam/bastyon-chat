f717e7eddd94f1529a88b0919bfd8102fce2b44d
Maksim Muhamedzyanov <maksim@Maksims-MacBook-Pro.local>
2022-09-24 13:44:49 +0500

    remove mnemonic key

--- public/js/decryptMnemonicPhrase.js
+++ public/js/decryptMnemonicPhrase.js
@@ -1,14 +1,12 @@
 const iv = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34];
-const testMnemonicPhrase = 'a5caf8773a37030632215bfd4049a9cf11a6987d36f775ed391ff4bdfeb56c8960a041337303f9aa03285880b0425a9ea2a80eb0efa6e11c1f5ec65faff1154550165dc5b828dccb2e28c3ad09e3edc4'
-const testPrivateMnemonicPhrase = '99e4df2f0588793927f935e63ffdb5044f66c39ee63d6bfe57da531a3cbf17a28da1427a8934303d7398496fbea56156769b3ae79405b8be3328944490df47640ac692cbac0f4fe20488e1d0f979e349'
-const testWif = '26b37f3206c710d62f9c4bc6e32750b9f6c4d142c7581d7c833135fcc976f2a73d93289f7e57a6fc4137b32077758015d0f009b7a3d6f1911ee86afc3e9c39f1'
+
 const localStoreMnemonicPhrase = localStorage.getItem('mnemonic') ? localStorage.getItem('mnemonic') : '';
 let privateKey = null;
 let publicKey = null;
 let addressUser = null;
 
 function decryptMnemonic() {
-    const mnemonicPhrase = localStoreMnemonicPhrase ? localStoreMnemonicPhrase : testMnemonicPhrase;
+    const mnemonicPhrase = localStoreMnemonicPhrase;
 
     decryption(mnemonicPhrase, hexEncode('fakefingerprint'), {}, function(mnemonicPhrase){
         (!window.bitcoin.bip39.validateMnemonic(mnemonicPhrase) 
