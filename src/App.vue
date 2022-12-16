145ba1eebe4fc31139f6d731f3a7acdf167d7487
max-kondratyuk <118020839+max-kondratyuk@users.noreply.github.com>
2022-12-16 21:56:33 +0200

    fix lints

--- src/App.vue
+++ src/App.vue
@@ -548,210 +548,176 @@ export default {
     this.$store.commit('setPocketnet', this.pocketnet);
     this.$store.commit('setMobile', this.mobile);
     this.$store.commit('setVoiceMessagesEnabled', this.recording);
+    this.$store.commit('pkoindisabled', this.pkoindisabled)
     this.$store.commit('clearall');
 
-    this.$store.commit("ls");
+    this.$store.commit('ls');
 
-    this.importInitialScripts();
+    this.importInitialScripts()
 
     this.generateTeamroomMessages();
     setTimeout(() => {
-      if (this.fcmtoken && window.cordova) this.setPusher(this.fcmtoken);
+      if (this.fcmtoken && window.cordova)
+        this.setPusher(this.fcmtoken);
     }, 5000);
 
     var testUsers = {
       matrixMan: {
-        address: f.hexEncode("PToMRMsMVh9dj4Cpa7yu1pB5iq65g4jrVC"),
-        privateKey:
-          "510aa931cdee36b9ac467ad04a2619f46be88815812b46e976e36bc8dbcdc41b",
+        address: f.hexEncode('PToMRMsMVh9dj4Cpa7yu1pB5iq65g4jrVC'),
+        privateKey: '510aa931cdee36b9ac467ad04a2619f46be88815812b46e976e36bc8dbcdc41b'
       },
       ltme: {
-        address: f.hexEncode("PKszvjHwMWubQpeEKnQhRonGBVFWcJDbNJ"),
-        privateKey:
-          "513bc6a5014e7dd203537227fb0a78f7b974853478fcf95103125f6d793a5f8b",
+        address: f.hexEncode('PKszvjHwMWubQpeEKnQhRonGBVFWcJDbNJ'),
+        privateKey: '513bc6a5014e7dd203537227fb0a78f7b974853478fcf95103125f6d793a5f8b'
       },
       travisnoscot1: {
-        address: f.hexEncode("PDjvHUQjcDX8Vq6rtpKveUDPtwPVUZEW7m"),
-        privateKey:
-          "63d91d001f7b5167a707dd4ee7e2638fc13e1fa3ff7171b040b432bcd1054e28",
+        address: f.hexEncode('PDjvHUQjcDX8Vq6rtpKveUDPtwPVUZEW7m'),
+        privateKey: '63d91d001f7b5167a707dd4ee7e2638fc13e1fa3ff7171b040b432bcd1054e28'
       },
       mobRoocky: {
-        address: f.hexEncode("PCMnk8T3Edu81iGcTEpVCFq8FQd6UBXi2y"),
-        privateKey:
-          "0bbf6fb0dbf78a26f00345f6f96d4f76f52e0093417c51a1b7bccb7ca92e55df",
+        address: f.hexEncode('PCMnk8T3Edu81iGcTEpVCFq8FQd6UBXi2y'),
+        privateKey: '0bbf6fb0dbf78a26f00345f6f96d4f76f52e0093417c51a1b7bccb7ca92e55df'
       },
       user1: {
-        address: f.hexEncode("PKxqtUuK3B6qwAxs6HLhvxrQVExuMwKWd5"),
-        privateKey:
-          "28f0fdc30094d517bc08abca3f219d77fc32230e313bbf0b699ecd42f80de82a0",
+        address: f.hexEncode('PKxqtUuK3B6qwAxs6HLhvxrQVExuMwKWd5'),
+        privateKey: '28f0fdc30094d517bc08abca3f219d77fc32230e313bbf0b699ecd42f80de82a0'
       },
       user2: {
-        address: f.hexEncode("PGe4xVAJRJtbD8swPipE2ug9Zk1rhSTa1y"),
-        privateKey:
-          "0b4562275da7e60a57e10432bdc7e3318dd9699b8b928224ba360b9fb951c77b",
+        address: f.hexEncode('PGe4xVAJRJtbD8swPipE2ug9Zk1rhSTa1y'),
+        privateKey: '0b4562275da7e60a57e10432bdc7e3318dd9699b8b928224ba360b9fb951c77b'
       },
       FakeBadash: {
-        address: f.hexEncode("PDWPDFikMhLJHtiKLLXZGzfgphRcd19eHe"),
-        privateKey:
-          "99d8ce99383ccc5d6404535b98f2b43ce5ab939a28bfc52a442565b2351039fa",
+        address: f.hexEncode('PDWPDFikMhLJHtiKLLXZGzfgphRcd19eHe'),
+        privateKey: '99d8ce99383ccc5d6404535b98f2b43ce5ab939a28bfc52a442565b2351039fa'
       },
       Faketravis: {
-        address: f.hexEncode("PGURYzvoXuSvuwCuDty2wW37omjiKgzV5a"),
-        privateKey:
-          "93641f20095aa15b89b78d395ebe0a5a9b149ea2b54c57d896091c0ea9b86dfd",
+        address: f.hexEncode('PGURYzvoXuSvuwCuDty2wW37omjiKgzV5a'),
+        privateKey: '93641f20095aa15b89b78d395ebe0a5a9b149ea2b54c57d896091c0ea9b86dfd'
       },
       fakeRocky: {
-        address: f.hexEncode("PRDKVP4yvF2wW8p5FUVkzrCvXYE8SMGkQK"),
-        privateKey:
-          "80d213d272405d4ff51752f91e79c639919f5f9f302583bdaa1c767b044ce2c0",
+        address: f.hexEncode('PRDKVP4yvF2wW8p5FUVkzrCvXYE8SMGkQK'),
+        privateKey: '80d213d272405d4ff51752f91e79c639919f5f9f302583bdaa1c767b044ce2c0'
       },
       fakeFuture2: {
-        address: "PBHvKTH5TGQYDbRHgQHTTvaBf7tuww6ho7",
-        privateKey:
-          "71cacb25f90b18e356e33bc12c428c2e8b6b7c6e2950ab6344412f226aab0b2c",
+        address: 'PBHvKTH5TGQYDbRHgQHTTvaBf7tuww6ho7',
+        privateKey: '71cacb25f90b18e356e33bc12c428c2e8b6b7c6e2950ab6344412f226aab0b2c'
       },
       fakeFuture: {
-        address: f.hexEncode("PBHvKTH5TGQYDbRHgQHTTvaBf7tuww6ho7"),
-        privateKey:
-          "71cacb25f90b18e356e33bc12c428c2e8b6b7c6e2950ab6344412f226aab0b2c",
+        address: f.hexEncode('PBHvKTH5TGQYDbRHgQHTTvaBf7tuww6ho7'),
+        privateKey: '71cacb25f90b18e356e33bc12c428c2e8b6b7c6e2950ab6344412f226aab0b2c'
       },
       encMan: {
-        address: f.hexEncode("PEkKrb7WJgfU3rCkkU9JYT8jbGiQsw8Qy8"),
-        privateKey:
-          "616fd57f7610288978f4c01797a90d56b0c30392e598a2504b98c7a7246569a5",
+        address: f.hexEncode('PEkKrb7WJgfU3rCkkU9JYT8jbGiQsw8Qy8'),
+        privateKey: '616fd57f7610288978f4c01797a90d56b0c30392e598a2504b98c7a7246569a5'
       },
       harisCalvin: {
-        address: f.hexEncode("PXEWRce8siwbQWArQWyjnooogAEieHwhVd"),
-        privateKey:
-          "218bcb76df20438c1431c0394a42947f78f279bea987d7b238bfbd1cb1533b62",
+        address: f.hexEncode('PXEWRce8siwbQWArQWyjnooogAEieHwhVd'),
+        privateKey: '218bcb76df20438c1431c0394a42947f78f279bea987d7b238bfbd1cb1533b62'
       },
       oneSomeoneNigga: {
-        address: f.hexEncode("PGyjEoqmmK2tm6xmg69hGoSNM6hUrc4VfE"),
-        privateKey:
-          "254f5b9394f0490e78b41c6bf615bfd8e290fde3fb6d862f166b181b35764b06",
+        address: f.hexEncode('PGyjEoqmmK2tm6xmg69hGoSNM6hUrc4VfE'),
+        privateKey: '254f5b9394f0490e78b41c6bf615bfd8e290fde3fb6d862f166b181b35764b06'
       },
       gomer: {
-        address: f.hexEncode("PVSdoFQqAg86p6tbAW8Y6CGGGjhiXkr4qh"),
-        privateKey:
-          "105de1935fa665bdc23b1b02bcdb3f62e01a3ad48823d5348edb5b94ec1b4526",
+        address: f.hexEncode('PVSdoFQqAg86p6tbAW8Y6CGGGjhiXkr4qh'),
+        privateKey: '105de1935fa665bdc23b1b02bcdb3f62e01a3ad48823d5348edb5b94ec1b4526'
       },
       bart: {
-        address: f.hexEncode("PWioUw52q4iNpMQUauNYaWg9JpsPE1u9Ut"),
-        privateKey:
-          "a74bc4ee63290dd051d659fe46962176d2b9193e04104dea63116d25703bad72",
+        address: f.hexEncode('PWioUw52q4iNpMQUauNYaWg9JpsPE1u9Ut'),
+        privateKey: 'a74bc4ee63290dd051d659fe46962176d2b9193e04104dea63116d25703bad72'
       },
       edSheeran: {
-        address: f.hexEncode("PVeqjqJk2GthxBK6QdhCCfQVEX1SAK92As"),
-        privateKey:
-          "d89952018721e6762de13fa2f1891b0dc2d1f1e75402ba483251086b2ba181ac",
+        address: f.hexEncode('PVeqjqJk2GthxBK6QdhCCfQVEX1SAK92As'),
+        privateKey: 'd89952018721e6762de13fa2f1891b0dc2d1f1e75402ba483251086b2ba181ac'
       },
       duftPunkMan: {
-        address: f.hexEncode("PFnN8SExxLsUjMKzs2avdvBdcA3ZKXPPkF"),
-        privateKey:
-          "7d1a31cc49a05692cfd992359cd27e8b3783cd186d98053ea0b9307b00a19018",
+        address: f.hexEncode('PFnN8SExxLsUjMKzs2avdvBdcA3ZKXPPkF'),
+        privateKey: '7d1a31cc49a05692cfd992359cd27e8b3783cd186d98053ea0b9307b00a19018'
       },
       jcole: {
-        address: f.hexEncode("P93cWU7X31wqn3R9GcUZ7c3fD11asXum5y"),
-        privateKey:
-          "900099127ad048d64523c4701d72d67b5b7f2afb18debbfdc53587f04a29c808",
+        address: f.hexEncode('P93cWU7X31wqn3R9GcUZ7c3fD11asXum5y'),
+        privateKey: '900099127ad048d64523c4701d72d67b5b7f2afb18debbfdc53587f04a29c808'
       },
       flowMan: {
-        address: f.hexEncode("PM4gVCcWnTmysUXhMukoQA2mT7GpfWQvck"),
-        privateKey:
-          "12d28e4c950193665dcaaa32129c061b2213e736ffe20918f0f13b6a2857cc32",
+        address: f.hexEncode('PM4gVCcWnTmysUXhMukoQA2mT7GpfWQvck'),
+        privateKey: '12d28e4c950193665dcaaa32129c061b2213e736ffe20918f0f13b6a2857cc32'
       },
       JokeMan: {
-        address: f.hexEncode("PMSULgjrsCsEnWgNigCga5jSjhCvYBFSA8"),
-        privateKey:
-          "f7a2518393db3779a4394f4084473847bc4260a5aad20859eff9d32ab03c310e",
+        address: f.hexEncode('PMSULgjrsCsEnWgNigCga5jSjhCvYBFSA8'),
+        privateKey: 'f7a2518393db3779a4394f4084473847bc4260a5aad20859eff9d32ab03c310e'
       },
       omgMan: {
-        address: f.hexEncode("P9R3aNvyjRbqTWfy5q1h5qvXFYaDtqaRi2"),
-        privateKey:
-          "80b4350df8c033a63fd7c6b71516ae05ab3a23a604a39afba5874b87b4effbf8",
+        address: f.hexEncode('P9R3aNvyjRbqTWfy5q1h5qvXFYaDtqaRi2'),
+        privateKey: '80b4350df8c033a63fd7c6b71516ae05ab3a23a604a39afba5874b87b4effbf8'
       },
       nineBot: {
-        address: f.hexEncode("PNcvYsMXzf4RVmS9AjRYEqeeGjk1PiDjvF"),
-        privateKey:
-          "447554f94a3512a420ef6d089411dff3bc6d170f65e246cc52fc30aa9ed2216c",
+        address: f.hexEncode('PNcvYsMXzf4RVmS9AjRYEqeeGjk1PiDjvF'),
+        privateKey: '447554f94a3512a420ef6d089411dff3bc6d170f65e246cc52fc30aa9ed2216c'
       },
       tenBot: {
-        address: f.hexEncode("PS9F8hzGSxeL1Lc2VGvPoqXHotZvpoVkkX"),
-        privateKey:
-          "659bd118d33e541bb042015723cab1e6b644a9d330a988b10e6d593f9abe3542",
+        address: f.hexEncode('PS9F8hzGSxeL1Lc2VGvPoqXHotZvpoVkkX'),
+        privateKey: '659bd118d33e541bb042015723cab1e6b644a9d330a988b10e6d593f9abe3542'
       },
       bikeBot: {
-        address: f.hexEncode("PBK1GDpiLNVFrKsPVAaNLU92byFY5P5KBr"),
-        privateKey:
-          "4c1ee4ae821a1ca885d77db84518b4dcbcd4efbed69603c8706b9557618529f9",
+        address: f.hexEncode('PBK1GDpiLNVFrKsPVAaNLU92byFY5P5KBr'),
+        privateKey: '4c1ee4ae821a1ca885d77db84518b4dcbcd4efbed69603c8706b9557618529f9'
       },
       angryBot: {
-        address: f.hexEncode("PBMcLRBQNWtNQZYUbRZZAd5FuZ8yb7HMX2"),
-        privateKey:
-          "87bec25e930671a06e032dca484083d4b79e23aa985781995882d5d4594f8d8d",
+        address: f.hexEncode('PBMcLRBQNWtNQZYUbRZZAd5FuZ8yb7HMX2'),
+        privateKey: '87bec25e930671a06e032dca484083d4b79e23aa985781995882d5d4594f8d8d'
       },
 
       test099090: {
-        address: f.hexEncode("PP582V47P8vCvXjdV3inwYNgxScZCuTWsq"),
-        privateKey:
-          "14ede296d13f8111c77a1da82519939329ee1b81ab23203da48df3f5e52fee3d",
+        address: f.hexEncode('PP582V47P8vCvXjdV3inwYNgxScZCuTWsq'),
+        privateKey: '14ede296d13f8111c77a1da82519939329ee1b81ab23203da48df3f5e52fee3d'
       },
       tony: {
-        address: f.hexEncode("TLENU8HFyz8PqzpKBDWeN6Xix6rM8wjYKd"),
-        privateKey:
-          "8cf816c1c7a51cbc114db9aac8896b8654260f9b3a985232f27cf32823c7f45f",
+        address: f.hexEncode('TLENU8HFyz8PqzpKBDWeN6Xix6rM8wjYKd'),
+        privateKey: '8cf816c1c7a51cbc114db9aac8896b8654260f9b3a985232f27cf32823c7f45f'
       },
       invoker: {
-        address: f.hexEncode("TPjN8Dy4BpGDezMNw2ePuEdHiHetTvR3Mb"),
-        privateKey:
-          "a7ded16bd9d3350d6c3d233e6905b51e61bd879ba152ae24d0f8def76c4581c7",
+        address: f.hexEncode('TPjN8Dy4BpGDezMNw2ePuEdHiHetTvR3Mb'),
+        privateKey: 'a7ded16bd9d3350d6c3d233e6905b51e61bd879ba152ae24d0f8def76c4581c7'
       },
       drowRanger: {
-        address: f.hexEncode("TU8fAf96kSfQzmxmpjRDSSqvCiPWzWt67J"),
-        privateKey:
-          "38484a2047b5c8170847a97fc5cddf3d485c934c6532a51a73f64ec7e4659891",
+        address: f.hexEncode('TU8fAf96kSfQzmxmpjRDSSqvCiPWzWt67J'),
+        privateKey: '38484a2047b5c8170847a97fc5cddf3d485c934c6532a51a73f64ec7e4659891'
       },
       nevermore: {
-        address: f.hexEncode("TKJ7xAC18XC4zQBvkNAWhkr3i7cARu11Uv"),
-        privateKey:
-          "8ec7f71d16b07d29bb49b3bc3054b5a4d2e65fb559e0ae695503568227584f53",
+        address: f.hexEncode('TKJ7xAC18XC4zQBvkNAWhkr3i7cARu11Uv'),
+        privateKey: '8ec7f71d16b07d29bb49b3bc3054b5a4d2e65fb559e0ae695503568227584f53'
       },
       stormSpirit: {
-        address: f.hexEncode("TZBHmnZwLedPtHcJfJnFsbxobGuDKGf9uA"),
-        privateKey:
-          "642aaa706fe94ea77239468cb9aad6b463472793a2c4e1f60acf22078770c45f",
+        address: f.hexEncode('TZBHmnZwLedPtHcJfJnFsbxobGuDKGf9uA'),
+        privateKey: '642aaa706fe94ea77239468cb9aad6b463472793a2c4e1f60acf22078770c45f'
       },
       morph: {
-        address: f.hexEncode("TXyieuLsCntT1p4ougtiKURtYTxcdvWN1L"),
-        privateKey:
-          "8f59a6a6b7b8c1f63be47a6f571a11d726e9d7cde2baff52a51dc72326b6abb4",
+        address: f.hexEncode('TXyieuLsCntT1p4ougtiKURtYTxcdvWN1L'),
+        privateKey: '8f59a6a6b7b8c1f63be47a6f571a11d726e9d7cde2baff52a51dc72326b6abb4'
       },
       alchemist: {
-        address: f.hexEncode("TT2CeQuHiM8E8hG4rkJcaMKQ8gWqhZmNUH"),
-        privateKey:
-          "957ad5e2a009e4c0f06f0ae2737ea4c36c48fa8312555fcd104ca9cff7db6848",
+        address: f.hexEncode('TT2CeQuHiM8E8hG4rkJcaMKQ8gWqhZmNUH'),
+        privateKey: '957ad5e2a009e4c0f06f0ae2737ea4c36c48fa8312555fcd104ca9cff7db6848'
       },
       public_user_test: {
-        address: f.hexEncode("TLMXdC2UYSfMVbLzKtmJJTi4oK9LNHNVju"),
-        privateKey:
-          "509b219c5cead9545039ae38d428813af6671dbee3e37cadd2cd22f152013f2a",
+        address: f.hexEncode('TLMXdC2UYSfMVbLzKtmJJTi4oK9LNHNVju'),
+        privateKey: '509b219c5cead9545039ae38d428813af6671dbee3e37cadd2cd22f152013f2a'
       },
       cleanUser: {
-        address: f.hexEncode("THCRHFtVAoeDGLg548C4bQxXeDg9bwrs5K"),
-        privateKey:
-          "666919a742eb63bededea3fddc44b526148d8b0eb1126b0aaff480a2af888d92",
+        address: f.hexEncode('THCRHFtVAoeDGLg548C4bQxXeDg9bwrs5K'),
+        privateKey: '666919a742eb63bededea3fddc44b526148d8b0eb1126b0aaff480a2af888d92'
       },
       userwithoutavatar: {
-        address: f.hexEncode("TVcPMgVQ9TxgpzNrQyEAei5cnnR9jzXtzE"),
-        privateKey:
-          "2a3e96562c23a40e3d439b823453fb8fe6f8fb2c0267e8d2411734b3bdff90dd",
+        address: f.hexEncode('TVcPMgVQ9TxgpzNrQyEAei5cnnR9jzXtzE'),
+        privateKey: '2a3e96562c23a40e3d439b823453fb8fe6f8fb2c0267e8d2411734b3bdff90dd'
       },
       unnamed: {
-        address: f.hexEncode("THsA8rwdeAkq5bePA71Nw5CGtFg2duyDSq"),
-        privateKey:
-          "27b42dfba3d20ae7a945e09dd0688137fa7963fd48b94f7b4027dc4eed874a96",
+        address: f.hexEncode('THsA8rwdeAkq5bePA71Nw5CGtFg2duyDSq'),
+        privateKey: '27b42dfba3d20ae7a945e09dd0688137fa7963fd48b94f7b4027dc4eed874a96'
       },
-    };
+
+
+    }
 
     var actualUser = {
       address: this.address ? f.hexEncode(this.address) : "",
