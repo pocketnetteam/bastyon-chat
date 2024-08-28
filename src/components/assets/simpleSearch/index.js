791b240e7e2d5bf68f6bc159ad7e6f8c7cf63c3b
Daniil Kim <73555295+DaniilKimlb@users.noreply.github.com>
2024-08-28 13:34:25 +0600

    fix: resolved text duplication issue and cleaned up the search component (#125)
    - Removed duplicated methods `blured` and `focused`
    - Added checks for ref existence in `clickicon` and `focus` methods
    - Removed unused `created` lifecycle hook
    - Simplified state synchronization between `searchTxt` and `value`

--- src/components/assets/simpleSearch/index.js
+++ src/components/assets/simpleSearch/index.js
@@ -1,112 +1,101 @@
-import {mapState} from 'vuex';
-// TODO IGOR' SEARCH ALL COMPONENTS
+import { mapState } from "vuex";
+
 export default {
-	name: 'search',
+	name: "search",
 	props: {
 		placeholder: String,
 		minimize: {
 			type: Boolean,
-			default: false
+			default: false,
 		},
-
-		loading : Boolean,
-		activity : String,
-		value : String,
-
-		controlKeys : Boolean
+		loading: Boolean,
+		activity: String,
+		value: String,
+		controlKeys: Boolean,
 	},
 
 	data: function () {
-
 		return {
 			isTyping: false,
-			searchTxt: ''
-		}
-
-	},
-
-	created: () => {
-
+			searchTxt: this.value || "",
+		};
 	},
 
 	watch: {
-		searchTxt : {
-			handler : _.debounce(function() {
-
-				var text = this.searchTxt
-				this.$emit('search', text)
-	
-			}, 500)
+		searchTxt: {
+			handler: _.debounce(function () {
+				var text = this.searchTxt;
+				if (text !== this.value) {
+					this.$emit("input", text);
+					this.$emit("search", text);
+				}
+			}, 500),
 		},
 
-		value : {
-			immediate : true,
-			handler : function(value){
-
-				if(value != this.searchTxt)
-					this.searchTxt = value
-	
-			}
-		}
+		value: {
+			immediate: true,
+			handler: function (value) {
+				if (value !== this.searchTxt) {
+					this.searchTxt = value;
+				}
+			},
+		},
 	},
+
 	computed: mapState({
-		auth: state => state.auth,
-		pocketnet: state => state.pocketnet,
-		minimized: state => state.minimized,
+		auth: (state) => state.auth,
+		pocketnet: (state) => state.pocketnet,
+		minimized: (state) => state.minimized,
 		active: function (state) {
-			return state.active || !this.minimize
+			return state.active || !this.minimize;
 		},
 	}),
 
 	methods: {
-		change : function(event){
-			this.searchTxt = event.target.value
+		change: function (event) {
+			this.searchTxt = event.target.value;
 		},
 
-		clear : function(event){
-			this.searchTxt = ''
-		}, 
- 
+		clear: function () {
+			this.searchTxt = "";
+			this.$emit("input", "");
+		},
+
 		clickicon: function () {
-			this.$refs['input'].focus()
+			if (this.$refs["input"]) {
+				this.$refs["input"].focus();
+			}
 		},
 
-		blured: function () {
-		},
-
-		focused: function () {
-		},
-
-		focus : function(){
-			this.$refs['input'].focus()
+		focus: function () {
+			if (this.$refs["input"]) {
+				this.$refs["input"].focus();
+			}
 		},
 
 		keydown(event) {
+			let e = false;
 
-			var e = false
-
-			if(this.controlKeys){
+			if (this.controlKeys) {
 				if (event.keyCode === 40 || event.keyCode === 38) {
-					this.$emit('controlKey', event.keyCode === 40 ? 'down' : 'up')
-					e = true
+					this.$emit("controlKey", event.keyCode === 40 ? "down" : "up");
+					e = true;
 				}
 
 				if (event.keyCode === 39 || event.keyCode === 13) {
-					this.$emit('controlKey', 'enter')
-					e = true
+					this.$emit("controlKey", "enter");
+					e = true;
 				}
-
 			}
 
-			if(e) {
-				event.preventDefault()
-
-				return false
+			if (e) {
+				event.preventDefault();
+				return false;
 			}
 		},
 
-		paste : function(evt){
-			this.$emit('paste', evt)
+		paste: function (evt) {
+			this.$emit("paste", evt);
 		},
 
 		blured: function () {
@@ -115,6 +104,6 @@ export default {
 
 		focused: function () {
 			this.$store.commit("blockactive", { value: true, item: "input" });
-		}
+		},
 	},
-}
+};
