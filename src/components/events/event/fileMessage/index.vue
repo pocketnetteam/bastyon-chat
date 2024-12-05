bb76c07d9172d6f242cb2ddba74c90b0e590b402
Daniil Kim <73555295+DaniilKimlb@users.noreply.github.com>
2024-12-05 15:18:44 +0500

    feat: file upload error handling to computed property (#141)

--- src/components/events/event/fileMessage/index.vue
+++ src/components/events/event/fileMessage/index.vue
@@ -9,40 +9,11 @@
 				<span>{{ humanReadableSize(file.size) }}</span>
 			</div>
 
-			<!--<div class="download" v-if="!encryptedData">
-				<a
-					:href="file.url"
-					:download="download"
-					target="_external"
-					class="fileMessage"
-				>
-					<button class="button small rounded">
-						<span v-if="!downloaded">{{ $t("button.download") }}</span>
-						<span v-else>{{ $t("button.downloaded") }}</span>
-					</button>
-				</a>
+			<div v-if="errorMessage" class="error">
+				<span v-if="isErrorShort">{{ errorMessage }}</span>
+				<span v-else>{{ $t("error_generic") }}</span>
 			</div>
-
-			<div class="download" >
-				<button class="button small rounded" @click="download">
-					<span v-if="!downloaded">{{ $t("button.download") }}</span>
-					<span v-else>{{ $t("button.downloaded") }}</span>
-				</button>
-			</div>-->
-			<template v-if="canDownload">
-				<!--<div class="download" v-if="!encryptedData">
-					<a
-						:href="file.url"
-						:download="download"
-						target="_external"
-						class="fileMessage"
-					>
-						<button class="button small rounded">
-							<span v-if="!downloaded">{{ $t("button.download") }}</span>
-							<span v-else>{{ $t("button.downloaded") }}</span>
-						</button>
-					</a>
-				</div>-->
+			<template v-else-if="canDownload">
 				<div class="download">
 					<button class="button small rounded" @click="download">
 						<span v-if="!downloaded">{{ $t("button.download") }}</span>
@@ -51,6 +22,7 @@
 				</div>
 			</template>
 		</div>
+
 		<div class="icon">
 			<a
 				:href="file.url"
@@ -63,6 +35,7 @@
 		</div>
 	</div>
 </template>
+
 <script>
 import f from "@/application/functions";
 
@@ -72,17 +45,15 @@ export default {
 		file: Object,
 		canDownload: {
 			type: Boolean,
-			default: true,
+			default: true
 		},
+		errorMessage: String,
 		encryptedData: Boolean,
-		downloaded: Boolean,
+		downloaded: Boolean
 	},
 	computed: {
-		getExtension: function (file) {
-			var name = file.name.split(".");
-			var ext = name[name.length - 1].toLowerCase();
-
-			return ext;
+		isErrorShort() {
+			return this.errorMessage.length <= 35;
 		},
 	},
 	methods: {
@@ -104,7 +75,6 @@ export default {
 		vertical-align: middle
 
 	.icon
-
 		width: 60px
 		text-align: center
 		background: srgb(--neutral-grad-0)
@@ -124,14 +94,18 @@ export default {
 			white-space: nowrap
 
 		.size
-			color : srgb(--color-txt-ac)
+			color: srgb(--color-txt-ac)
 
 		button.button
-			padding : 0.5 * $r
-			margin-top : $r
+			padding: 0.5 * $r
+			margin-top: $r
 
 			&.downloaded
 				background: srgb(--color-good)
-				border-color : srgb(--color-good)
-				color : srgb(--text-on-bg-ac-color)
+				border-color: srgb(--color-good)
+				color: srgb(--text-on-bg-ac-color)
+
+
+.error
+	color: rgb(var(--color-bad))
 </style>
