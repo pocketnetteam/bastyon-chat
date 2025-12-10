1fb6deaa8d684d966fb10b49f69c80a25da55fb5
daniil <dan.kim.04lb@gmail.com>
2025-12-11 02:29:28 +0900

    feat: add delete chats

--- src/application/mtrxkit.js
+++ src/application/mtrxkit.js
@@ -17,39 +17,76 @@ class MTRXKIT {
 		var users = this.core.mtrx.chatUsersInfo(m_chat.roomId);
 		var tt = false;
 
-		
-
 		if (users.length == 2) {
+			var tid = this.tetatetid(users[0], users[1]);
 
-			var tid = this.tetatetid(users[0], users[1])
-
-			tt = m_chat.name == "#" + tid || (m_chat.getCanonicalAlias() || "").indexOf(tid) > -1;
+			tt =
+				m_chat.name == "#" + tid ||
+				(m_chat.getCanonicalAlias() || "").indexOf(tid) > -1;
 		}
 		if (users.length > 1) m_chat.tetatet = tt;
 
 		return tt;
 	}
+	canInteractWithRoom(room) {
+		const interactiveTypes = ["join", "invite"];
+		return interactiveTypes.includes(room.selfMembership);
+	}
+	findOneToOneRoom(user1Id, user2Id) {
+		const rooms = Object.values(this.core.mtrx.store.rooms);
+		const targetUserIds = [
+			this.core.user.matrixId(user1Id),
+			this.core.user.matrixId(user2Id)
+		].sort();
 
-	tetatetid(user1, user2) {
+		const isRoomValid = room => {
+			return room.getJoinRule() !== "public" && this.canInteractWithRoom(room);
+		};
+
+		for (let room of rooms) {
+			if (!isRoomValid(room)) continue;
+
+			const members = room.getMembers();
+			if (members.length !== 2) continue;
+
+			const memberIds = members.map(member => member.userId).sort();
+			if (f.areArraysEqual(memberIds, targetUserIds)) {
+				return room.name.replace("#", "");
+			}
+		}
+	}
+	tetatetid(user1, user2, version) {
+		if (!version) {
+			const roomId = this.findOneToOneRoom(user1.id, user2.id);
+			if (roomId) {
+				return roomId;
+			}
+		}
+
 		var seed = 2;
 
 		if (user1.id == user2.id) return null;
 
-		var id = parseInt(user1.id, 16) * parseInt(user2.id, 16) * seed;
+		const ids = [user1.id, user2.id].sort();
+
+		var id = parseInt(ids[0], 16) * parseInt(ids[1], 16) * seed;
+		if (version) {
+			id += "-" + version;
+		}
 
 		if (cachestorage[id]) return cachestorage[id];
 
-		var hash = f.sha224(id.toString()).toString("hex");
+		var hash = f.sha224(id).toString("hex");
 
 		cachestorage[id] = hash;
 
 		return hash;
 	}
 
-	groupideq(users){
+	groupideq(users) {
 		var seed = 2;
 
-		var id = 1 * seed
+		var id = 1 * seed;
 
 		users = _.uniq(users, (u) => {
 			return u
@@ -79,10 +116,13 @@ class MTRXKIT {
 	groupid(users, owner) {
 		var seed = 2;
 
-		var id = users.reduce((s, u) => {
-			s += parseInt(u.id, 16);
-			return s;
-		}, 0) * parseInt(owner.id, 16) * seed;
+		var id =
+			users.reduce((s, u) => {
+				s += parseInt(u.id, 16);
+				return s;
+			}, 0) *
+			parseInt(owner.id, 16) *
+			seed;
 
 		if (cachestorage[id]) return cachestorage[id];
 
@@ -105,16 +145,14 @@ class MTRXKIT {
 	usersFromChats(m_chats) {
 		var users = {};
 
-		_.each(m_chats, (chat) => {
-			
-
+		_.each(m_chats, chat => {
 			users[chat.roomId] = _.map(
 				_.uniq(
 					[].concat(
 						_.toArray(chat.currentState.members),
 						chat.summary.members || []
 					),
-					(r) => {
+					r => {
 						return r.userId;
 					}
 				),
@@ -122,7 +160,7 @@ class MTRXKIT {
 					return {
 						userId: f.getmatrixid(r.userId),
 						membership: r.membership,
-						powerLevel: r.powerLevel,
+						powerLevel: r.powerLevel
 					};
 				}
 			);
@@ -132,9 +170,8 @@ class MTRXKIT {
 	}
 
 	prepareChat(m_chat) {
-
-		if(m_chat.getJoinRule() === "public"){
-			return Promise.resolve()
+		if (m_chat.getJoinRule() === "public") {
+			return Promise.resolve();
 		}
 
 		return this.usersInfoForChatsStore([m_chat]).then(() => {
@@ -143,26 +180,28 @@ class MTRXKIT {
 	}
 
 	prepareChatWithUsers(m_chat) {
-
-		if(m_chat.getJoinRule() === "public"){
-			return Promise.resolve()
+		if (m_chat.getJoinRule() === "public") {
+			return Promise.resolve();
 		}
 
-		return this.allchatmembers([m_chat], false, true).then((r) => {
+		return this.allchatmembers([m_chat], false, true).then(r => {
 			return this.prepareChat(m_chat);
-		})
+		});
 	}
 
 	fillContacts(m_chats) {
-		m_chats = _.filter(m_chats, (ch) => {
-			return ch.selfMembership == "join" && ch.name.length == 57 && ch.getJoinRule() != "public";
+		m_chats = _.filter(m_chats, ch => {
+			return (
+				ch.selfMembership == "join" &&
+				ch.name.length == 57 &&
+				ch.getJoinRule() != "public"
+			);
 		});
 
-		return this.usersInfoForChatsStore(m_chats).then((i) => {
-
+		return this.usersInfoForChatsStore(m_chats).then(i => {
 			this.core.store.commit(
 				"SET_CONTACTS_FROM_MATRIX",
-				_.filter(i, (m) => {
+				_.filter(i, m => {
 					return (
 						!this.core.user.userinfo || m.id !== this.core.user.userinfo.id
 					);
@@ -172,15 +211,13 @@ class MTRXKIT {
 	}
 
 	usersInfoForChatsStore(m_chats, reload) {
-
 		return this.usersInfoForChats(m_chats, reload)
-			.then((i) => {
-
+			.then(i => {
 				this.core.store.commit("SET_CHATS_USERS", this.usersFromChats(m_chats));
 
 				return Promise.resolve(i);
 			})
-			.catch((e) => {
+			.catch(e => {
 				return Promise.resolve();
 			});
 	}
@@ -206,27 +243,22 @@ class MTRXKIT {
 
 					return chat
 						.loadMembersFromServer()
-						.then((r) => {
-
-
+						.then(r => {
 							chat.summary.membersloading = false;
 
-							chat.summary.members = _.map(r, (user) => {
+							chat.summary.members = _.map(r, user => {
 								return {
 									name: f.getmatrixid(user.state_key),
 									membership: user.content.membership,
 									user: user,
 									userId: user.state_key,
-									powerLevel: user.content.powerLevel || 0,
+									powerLevel: user.content.powerLevel || 0
 								};
 							});
 
-							if (
-								chat.selfMembership === "invite" &&
-								this.core.user.userinfo
-							) {
+							if (chat.selfMembership === "invite" && this.core.user.userinfo) {
 								if (
-									!_.find(chat.summary.members, (m) => {
+									!_.find(chat.summary.members, m => {
 										return (
 											m.userId ==
 											this.core.user.matrixId(this.core.user.userinfo.id)
@@ -238,7 +270,7 @@ class MTRXKIT {
 										membership: "invite",
 										user: this.core.user.userinfo,
 										userId: this.core.user.matrixId(this.core.user.userinfo.id),
-										powerLevel: 0,
+										powerLevel: 0
 									});
 								}
 							}
@@ -276,8 +308,7 @@ class MTRXKIT {
 	usersInfoForChats(m_chats, reload) {
 		/// TODO FILTER CONTACTS
 
-		return this.allchatmembers(m_chats, reload).then((members) => {
-
+		return this.allchatmembers(m_chats, reload).then(members => {
 			return this.usersInfo(members, reload);
 		});
 	}
@@ -285,7 +316,7 @@ class MTRXKIT {
 	usersInfoById(id) {
 		var ids = [f.getmatrixid(id)];
 
-		return this.core.user.usersInfo(ids, false).then((r) => {
+		return this.core.user.usersInfo(ids, false).then(r => {
 			return Promise.resolve(r[0]);
 		});
 	}
@@ -305,7 +336,7 @@ class MTRXKIT {
 		let idForInviting = [];
 		let self = this;
 
-		users.forEach((user) => {
+		users.forEach(user => {
 			idForInviting.push(this.core.user.matrixId(user.id));
 		});
 
@@ -332,13 +363,13 @@ class MTRXKIT {
 		if (window.chatinvitedomains)
 			domains = [].concat(domains, window.chatinvitedomains);
 
-		_.each(domains, (domain) => {
-			_.each(ids, (id) => {
+		_.each(domains, domain => {
+			_.each(ids, id => {
 				idForInviting.push(this.core.user.matrixId(id, domain));
 			});
 		});
 
-		_.each(ids, (_id) => {
+		_.each(ids, _id => {
 			let idsForHash = parseInt(_id, 16);
 
 			id.push(idsForHash);
@@ -354,7 +385,7 @@ class MTRXKIT {
 	chatIsPublic(chat) {
 		var join_rules = chat.currentState.getStateEvents("m.room.join_rules");
 
-		return _.find(join_rules, (v) => {
+		return _.find(join_rules, v => {
 			return f.deep(v, "event.content.join_rule") == "public";
 		})
 			? true
